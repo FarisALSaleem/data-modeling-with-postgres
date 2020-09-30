d92fabcbb9e9d93fb0914cf1f37898eb4cd714b0
Faris ALSaleem <faris.al.saleem15@gamil.com>
2020-09-30 21:09:19 +0300

    correct the table schemas and add insert statements

--- sql_queries.py
+++ sql_queries.py
@@ -10,12 +10,12 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
 	CREATE TABLE songplays (
-		songplay_id int PRIMARY KEY,
+		songplay_id serial PRIMARY KEY,
 		start_time timestamp,
 		user_id int,
 		level varchar,
-		song_id int,
-		artist_id int,
+		song_id varchar,
+		artist_id varchar,
 		session_id int,
 		location varchar,
 		user_agent varchar
@@ -34,9 +34,9 @@ user_table_create = ("""
 
 song_table_create = ("""
 	CREATE TABLE songs (
-		song_id int PRIMARY KEY,
+		song_id varchar PRIMARY KEY,
 		title varchar,
-		artist_id int,
+		artist_id varchar,
 		year int,
 		duration float8
 	);
@@ -44,7 +44,7 @@ song_table_create = ("""
 
 artist_table_create = ("""
 	CREATE TABLE artists (
-		artist_id int PRIMARY KEY,
+		artist_id varchar PRIMARY KEY,
 		name varchar,
 		location varchar,
 		latitude float8,
@@ -60,31 +60,37 @@ time_table_create = ("""
 		week int,
 		month int,
 		year int,
-		weekday boolean
+		weekday int
 	);
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+	INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s ,%s)
 """)
 
 user_table_insert = ("""
+	INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""
+	INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""
+	INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 
 time_table_insert = ("""
+	INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
 song_select = ("""
+	SELECT songs.song_id, artists.artist_id FROM (songs JOIN artists on songs.artist_id = artists.artist_id) WHERE title=%s AND name=%s AND duration=%s
 """)
 
 # QUERY LISTS
