ec1894650541db00bbd57a770ef1fa342cfff3c4
Faris ALSaleem <faris.al.saleem15@gamil.com>
2020-09-30 06:27:17 +0300

    Add SQL CREATE and DROP statements

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,67 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+	CREATE TABLE songplays (
+		songplay_id int PRIMARY KEY,
+		start_time timestamp,
+		user_id int,
+		level varchar,
+		song_id int,
+		artist_id int,
+		session_id int,
+		location varchar,
+		user_agent varchar
+	);
 """)
 
 user_table_create = ("""
+	CREATE TABLE users (
+		user_id int PRIMARY KEY,
+		first_name varchar,
+		last_name varchar,
+		gender varchar,
+		level varchar
+	);
 """)
 
 song_table_create = ("""
+	CREATE TABLE songs (
+		song_id int PRIMARY KEY,
+		title varchar,
+		artist_id int,
+		year int,
+		duration float8
+	);
 """)
 
 artist_table_create = ("""
+	CREATE TABLE artists (
+		artist_id int PRIMARY KEY,
+		name varchar,
+		location varchar,
+		latitude float8,
+		longitude float8
+	);
 """)
 
 time_table_create = ("""
+	CREATE TABLE time (
+		start_time timestamp PRIMARY KEY,
+		hour int,
+		day int,
+		week int,
+		month int,
+		year int,
+		weekday boolean
+	);
 """)
 
 # INSERT RECORDS
