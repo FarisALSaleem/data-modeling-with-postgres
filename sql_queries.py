162a6dc1437355e31bb2458b09a110ab596acfaa
Faris ALSaleem <faris.al.saleem15@gamil.com>
2020-10-01 23:39:07 +0300

    Remove comments

--- sql_queries.py
+++ sql_queries.py
@@ -1,12 +1,9 @@
-# DROP TABLES
 
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
-
-# CREATE TABLES
 
 songplay_table_create = ("""
     CREATE TABLE songplays (
@@ -64,7 +61,6 @@ time_table_create = ("""
     );
 """)
 
-# INSERT RECORDS
 
 songplay_table_insert = ("""
     INSERT INTO songplays (start_time, user_id, level, song_id, artist_id,
@@ -97,7 +93,6 @@ time_table_insert = ("""
     ON CONFLICT DO NOTHING
 """)
 
-# FIND SONGS
 
 song_select = ("""
     SELECT songs.song_id, artists.artist_id
@@ -105,7 +100,6 @@ song_select = ("""
     WHERE title=%s AND name=%s AND duration=%s
 """)
 
-# QUERY LISTS
 
 create_table_queries = [time_table_create, user_table_create,
                         artist_table_create, song_table_create,
