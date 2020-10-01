e13152bc19693f30c420882b957d6051401f69d6
Faris ALSaleem <faris.al.saleem15@gamil.com>
2020-10-01 22:09:40 +0300

    Conform sql_queries to pep8

--- sql_queries.py
+++ sql_queries.py
@@ -75,7 +75,7 @@ songplay_table_insert = ("""
 user_table_insert = ("""
     INSERT INTO users (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO 
+    ON CONFLICT (user_id) DO
     UPDATE SET level=EXCLUDED.level
 """)
 
@@ -107,7 +107,7 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [time_table_create , user_table_create,
+create_table_queries = [time_table_create, user_table_create,
                         artist_table_create, song_table_create,
                         songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop,
